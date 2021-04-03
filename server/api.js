0ac08efd2c2e382469d0a5ff4cad147acfed2cac
jjmetty <60830082+jjmetty@users.noreply.github.com>
2021-04-03 13:46:46 -0500

    add crud operations
    added crud operations except for update

--- server/api.js
+++ server/api.js
@@ -5,21 +5,46 @@ const dboperations = require('./operations/dboperations');
 var express = require ('express');
 var bodyParser = require ('body-parser');
 var cors = require('cors');
-const { response } = require('express');
+const { response, request } = require('express');
 var app = express();
 var router = express.Router();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('./api', router);
+app.use('/api', router);
 
 
-router.route('.;customers').get((req,res) =>{
+//get customers 
+router.route('/customers').get((req,res) =>{
 
     dboperations.getCustomers().then(result => {
-        console.log(result);
-        response.json(result[0]);
+      //  console.log(result);
+        res.json(result[0]);
+    })
+
+})
+
+//get customer
+router.route('/customers/:id').get((req,res) =>{
+
+    dboperations.getCustomer(req.params.id).then(result => {
+     //   console.log(result);
+        res.json(result[0]);
+    })
+
+})
+
+
+
+//add a customer
+router.route('/addCustomer').post((req,res) =>{
+
+    let customer = {...req.body}
+
+    dboperations.addCustomer(customer).then(result => {
+       // console.log(result);
+        res.status(201).json(result);
     })
 
 })
@@ -27,13 +52,25 @@ router.route('.;customers').get((req,res) =>{
 
 
 
+//delete a customer
+router.route('/deleteCustomer/:id').get((req,res) => {
+
+    let customerID = req.params.id;
+
+    dboperations.deleteCustomer(customerID).then (result =>{
+        res.json(result);
+    })
+})
+
+//update customer, last crud and test and start on the other tables
+
+
+
 var port = process.env.PORT || 8000;
 app.listen(port);
-console.log('Application is running on port: ' + port);
+console.log('Server is running on port ' + port);
 
 
-dboperations.getCustomers().then(result => {
-    console.log(result);
-})
+
 
 //25:14
--- server/operations/dboperations.js
+++ server/operations/dboperations.js
@@ -14,6 +14,73 @@ async function getCustomers(){
     } 
 }
 
+//get one customer id
+async function getCustomer(customerID){
+    try{
+        let pool = await sql.connect(config);
+        let customer = await pool.request()
+            .input ('input_param', sql.Int, customerID)
+            .query("SELECT * from customer where customerID = @input_param");
+        return customer.recordsets;
+
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+
+//add customer
+async function addCustomer(customer){
+    try{
+        let pool = await sql.connect(config);
+        let insertCustomer = await pool.request()
+            .input ('customerID', sql.Int, customer.customerID)
+            .input ('customerTypeID', sql.Int, customer.customerTypeID)
+            .input ('customerRelationshipID', sql.Int, customer.customerRelationshipID)
+            .input ('streetNum', sql.Int, customer.streetNum)
+            .input ('unit',sql.VarChar, customer.unit)
+            .input ('prefix',sql.VarChar, customer.prefix)
+            .input ('streetName',sql.VarChar, customer.streetName)
+            .input ('suffix',sql.VarChar, customer.suffix)
+            .input ('unitNum',sql.Int, customer.unitNum)
+            .input ('city',sql.VarChar, customer.city)
+            .input ('stateID',sql.Int, customer.stateID)
+            .input ('zip',sql.Int, customer.zip)
+            .input ('zip4',sql.Int, customer.zip4)
+            .input ('customerNote',sql.VarChar, customer.customerNote)
+            .query('INSERT INTO [dbo].[customer] (customerID,customerTypeID,customerRelationshipID,streetNum,unit,prefix,streetName,suffix,unitNum,city,stateID,zip,zip4,customerNote) VALUES (@customerID, @customerTypeID, @customerRelationshipID,@streetNum,@unit,@prefix,@streetName,@suffix,@unitNum,@city,@stateID,@zip,@zip4,@customerNote)');
+           
+        return insertCustomer.recordsets;
+
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+
+//delete customer
+async function deleteCustomer(customerID){
+    try{
+        let pool = await sql.connect(config);
+        let deleted = await pool.request()
+            .input ('customerID', sql.Int, customerID)
+            .query('DELETE FROM [dbo].[customer] WHERE customerID = @customerID');
+        return deleted.recordsets;
+
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+
+
+
 module.exports ={
-    getCustomers : getCustomers
+    getCustomers : getCustomers,
+    getCustomer  : getCustomer,
+    addCustomer  : addCustomer,
+    deleteCustomer : deleteCustomer
 }
